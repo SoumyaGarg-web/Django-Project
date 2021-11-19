5abc5555ffa5a89b48b3eb9a5f2cde65abe1567c
soumya.garg1@payu.in <soumya.garg1@payu.in>
2021-11-19 16:22:55 +0530

    Calling Database functions 1

--- playground/views.py
+++ playground/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.db.models.aggregates import Count, Avg, Max, Min
 from store.models import Customer, Order, OrderItem, Product
 from django.db.models import Value
+from django.db.models.functions import Concat
 
 # Create your views here.
 
@@ -16,9 +17,13 @@ from django.db.models import Value
 
 def say_hello(request):
     # x = calculate()
+    # queryset = Customer.objects.annotate(
+    #     full_name=Func(F('first_name'), Value(
+    #         ' '), F('last_name'), function='CONCAT')
+    # )
+
     queryset = Customer.objects.annotate(
-        full_name=Func(F('first_name'), Value(
-            ' '), F('last_name'), function='CONCAT')
+        full_name=Concat('first_name', Value(' '), 'last_name')
     )
 
     return render(request, 'hello.html', {'name': 'Soumya', 'result': list(queryset)})
