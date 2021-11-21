cf52b4941527a4bac235bd78211e8b521c9e4c8c
soumya.garg1@payu.in <soumya.garg1@payu.in>
2021-11-21 19:04:35 +0530

    Custom Manager

--- tags/models.py
+++ tags/models.py
@@ -4,14 +4,31 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 
+
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        queryset = TaggedItem.objects.select_related('tag').filter(
+            content_type=content_type,
+            object_id=obj_id
+        )
+
+        return queryset
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
+
 
 class TaggedItem(models.Model):
     # what tag applied to what object.
     # Type(Product, Video, Article)
     # ID
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE) # table in which object can be found
-    object_id = models.PositiveIntegerField() # primary key id for that object
-    content_object =  GenericForeignKey() #read actual object for which particular tag is applied to
+    # table in which object can be found
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()  # primary key id for that object
+    # read actual object for which particular tag is applied to
+    content_object = GenericForeignKey()
