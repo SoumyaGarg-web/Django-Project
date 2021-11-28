5e602043755553b29dded3049063df8a6b76ba50
soumya.garg1@payu.in <soumya.garg1@payu.in>
2021-11-28 22:32:08 +0530

    Transaction 2

--- playground/views.py
+++ playground/views.py
@@ -10,11 +10,21 @@ from django.db.models.functions import Concat
 from django.contrib.contenttypes.models import ContentType
 from store.models import Product
 from tags.models import TaggedItem
+from django.db import transaction
 
 
 def say_hello(request):
 
-    collection = Collection(pk=1)
-    collection.delete()
+    with transaction.atomic():  # returns context manager
+        order = Order()
+        order.customer_id = 1
+        order.save()
+
+        item = OrderItem()
+        item.order = order
+        item.product_id = 1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
 
     return render(request, 'hello.html', {'name': 'Soumya'})
