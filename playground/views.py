7fc142d45340f916e0abc7d5ca9c8240d66bf0af
soumya.garg1@payu.in <soumya.garg1@payu.in>
2021-11-28 22:33:59 +0530

    Transaction 1

--- playground/views.py
+++ playground/views.py
@@ -13,18 +13,18 @@ from tags.models import TaggedItem
 from django.db import transaction
 
 
+@transaction.atomic()
 def say_hello(request):
 
-    with transaction.atomic():  # returns context manager
-        order = Order()
-        order.customer_id = 1
-        order.save()
+    order = Order()
+    order.customer_id = 1
+    order.save()
 
-        item = OrderItem()
-        item.order = order
-        item.product_id = 1
-        item.quantity = 1
-        item.unit_price = 10
-        item.save()
+    item = OrderItem()
+    item.order = order
+    item.product_id = 2
+    item.quantity = 1
+    item.unit_price = 10
+    item.save()
 
     return render(request, 'hello.html', {'name': 'Soumya'})
