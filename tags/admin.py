bae9d4f0ec28027e3e108e917feac9a81198a7b5
soumya.garg1@payu.in <soumya.garg1@payu.in>
2022-01-16 13:37:05 +0530

    Using generic relations admin

--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from store import models
+from .models import Tag
 # Register your models here.
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ['label']
